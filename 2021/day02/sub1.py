8284a80fa310a4597946fea8921af3ce87d3e13b
engjonah <engjonah@gmail.com>
2021-12-06 13:10:47 -0500

    Create sub1.py

---
+++ 2021/day02/sub1.py
@@ -0,0 +1,31 @@
+class Sub: 
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+    
+    def forward(self,x):
+        self.x +=x
+    
+    def down(self,x):
+        #self.y += x
+    
+    def up(self,x):
+        #self.y -= x
+    
+    def position(self):
+        return self.x, self.y, self.x * self.y
+
+
+my_sub = Sub()
+with open('input.txt', 'r') as file: 
+    for line in file: 
+        line = line.rstrip()
+        line = line.split()
+        if (line[0]=="forward"):
+            my_sub.forward(int(line[1]))
+        elif (line[0]=="down"):
+            my_sub.down(int(line[1]))
+        elif (line[0]=="up"):
+            my_sub.up(int(line[1]))
+
+print(my_sub.position()[2])
